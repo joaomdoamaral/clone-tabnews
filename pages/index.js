b3fb9e39907ad480ea7165f6c5b7352c7f439728
João Marcos do Amaral <91680827+joaomdoamaral@users.noreply.github.com>
2025-11-17 22:49:58 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Pam, você é o amor da minha vida! Se você me ama também dá uma risadinha...</h1>
+  return (
+    <h1>
+      Pam, você é o amor da minha vida! Se você me ama também dá uma
+      risadinha...
+    </h1>
+  );
+}
+
+function teste1() {
+  console.log("teste 1");
 }
 
 export default Home;
