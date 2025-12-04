655b7e8d39acd8d7927741cee9f5b5b71c81c4b0
João Marcos do Amaral <91680827+joaomdoamaral@users.noreply.github.com>
2025-12-04 19:09:18 +0000

    Teste msg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Pam, você é o amor da minha vida, nunca se esqueca disso!</h1>;
+  return <h1>Pam, você é o amor da minha vida, nunca se esqueça disso!</h1>;
 }
 
 export default Home;
